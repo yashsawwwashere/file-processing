d143b652197c3e25d65ab8ec5c244af9bead2bf2
yashsawwashere <yashsawwashere103@gmail.com>
2023-06-14 23:28:57 +0530

    enterprise project

---
+++ src/main/java/com/bootcoding/filehandiling/Enterprise.java
@@ -0,0 +1,103 @@
+package com.bootcoding.filehandiling;
+
+public class Enterprise {
+    private int year;
+    private  String industryA;
+    private  int industryC;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getIndustryA() {
+        return industryA;
+    }
+
+    public void setIndustryA(String industryA) {
+        this.industryA = industryA;
+    }
+
+    public int getIndustryC() {
+        return industryC;
+    }
+
+    public void setIndustryC(int industryC) {
+        this.industryC = industryC;
+    }
+
+    public String getIndustryName() {
+        return industryName;
+    }
+
+    public void setIndustryName(String industryName) {
+        this.industryName = industryName;
+    }
+
+    public String getUnits() {
+        return units;
+    }
+
+    public void setUnits(String units) {
+        this.units = units;
+    }
+
+    public String getVariable_name() {
+        return variable_name;
+    }
+
+    public void setVariable_name(String variable_name) {
+        this.variable_name = variable_name;
+    }
+
+    public String getVariable_code() {
+        return variable_code;
+    }
+
+    public void setVariable_code(String variable_code) {
+        this.variable_code = variable_code;
+    }
+
+    public String getVariable_category() {
+        return variable_category;
+    }
+
+    public void setVariable_category(String variable_category) {
+        this.variable_category = variable_category;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getIndustrycodeANZSIC() {
+        return industrycodeANZSIC;
+    }
+
+    public void setIndustrycodeANZSIC(String industrycodeANZSIC) {
+        this.industrycodeANZSIC = industrycodeANZSIC;
+    }
+
+    private  String industryName;
+    private  String units;
+    private  String variable_name;
+    private  String variable_code;
+    private  String variable_category;
+    private  int value;
+    private  String industrycodeANZSIC;
+
+
+
+
+
+
+
+
+}
