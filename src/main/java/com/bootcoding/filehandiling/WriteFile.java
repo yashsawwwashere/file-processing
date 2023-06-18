1f463512179fcb7a196fea7abe3879efb340d69a
yashsawwashere <yashsawwashere103@gmail.com>
2023-06-18 23:17:38 +0530

    enterprise project

---
+++ src/main/java/com/bootcoding/filehandiling/WriteFile.java
@@ -0,0 +1,43 @@
+//package com.bootcoding.filehandiling;
+//
+//import java.io.File;
+//import java.io.FileWriter;
+//import java.util.List;
+//import java.util.Random;
+//
+//public class WriteFile {
+//    public String generateRandomName() {
+//
+//        String[] name = {"prashil", "sakshit", "yash", "tanmay", "ved", "shantanu"};
+//
+//        Random r = new Random();
+//
+//        int bound = r.nextInt(name.length);
+//
+//        return name[bound];
+//        public void writeToFile(List<String>) {
+//            try {
+//                File file = new File("c://data//marks.txt");
+//
+//                file.createNewFile();
+//                FileWriter fw = new FileWriter(file);
+//                for(String d:data) {
+//                    fw.write(d);
+//
+//                }
+//                fw.close();
+//            } catch (Exception e) {
+//
+//            }
+//
+//
+//        }
+//
+//        public int generateRandomMarks(){
+//            Random random = new Random();
+//            return random.nextInt(35,99);
+//
+//        }
+//
+//
+//
